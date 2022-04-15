6a9683842c2ab79e282a1ffd3e03b310de5a1d37
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-04-16 00:04:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,5 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [vue()],
     publicPath: process.env.NODE_ENV === 'production' ?
-        '/my-project/' :
-        '/',
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src',
-                import.meta.url))
-        }
-    }
+        '/my-project/' : '/'
 })
