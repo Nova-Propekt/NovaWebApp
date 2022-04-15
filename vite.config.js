fe1dbfc6e4cd9c1cb423be030026e9ad3b255f72
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-04-16 00:17:21 +0200

    zg

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    publicPath: process.env.NODE_ENV === "production" ? "/NovaWebApp/" : "/",
     plugins: [vue()],
-    publicPath: process.env.NODE_ENV === 'production' ?
-        '/my-project/' : '/'
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src',
+                import.meta.url))
+        }
+    }
 })
