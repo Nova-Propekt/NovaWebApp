2805c083473aea4f74d8aba998920709747ca71a
FissionFeline <85946083+FissionFeline@users.noreply.github.com>
2022-05-02 23:43:33 +0200

    Fixed

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <h1 class="mainPurple">Nova Prospekt</h1>
-      <h3 >
-        Making simple things not a pain in the ass
-       <div class="text-center">
-    <v-btn
-      class="ma-2"
-      outlined
-      color="indigo"
-    >
-      Outlined Button
-    </v-btn>
-    <v-btn
-      class="ma-2"
-      outlined
-      fab
-      color="teal"
-    >
-      <v-icon>mdi-format-list-bulleted-square</v-icon>
-    </v-btn>
-    <v-btn
-      class="ma-2"
-      outlined
-      large
-      fab
-      color="indigo"
-    >
-      <v-icon>mdi-pencil</v-icon>
-    </v-btn>
-  </div>
-      </h3>
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <RouterView />
 </template>
 
 <style>
-@import './assets/base.css';
+@import '@/assets/base.css';
 
 #app {
   max-width: 1280px;
@@ -59,6 +33,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 header {
   line-height: 1.5;
+  max-height: 100vh;
 }
 
 .logo {
@@ -67,23 +42,47 @@ header {
 }
 
 a,
-.mainPurple {
+.green {
   text-decoration: none;
-  color: hsl(262, 43%, 49%);
+  color: hsla(160, 100%, 37%, 1);
   transition: 0.4s;
 }
 
 @media (hover: hover) {
   a:hover {
-    background-color: #7B7B7B
+    background-color: hsla(160, 100%, 37%, 0.2);
   }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
 }
 
 @media (min-width: 1024px) {
   body {
     display: flex;
     place-items: center;
-    background-color: #252525;
   }
 
   #app {
@@ -106,6 +105,15 @@ a,
 
   .logo {
     margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
